95d4efa7974c7cea5e1e500a724d9ab26f9de6fd
sanjeewa <sanjeewaapp@gmail.com>
2022-11-20 18:14:29 -0600

    Rename project name

--- examples/CalculateMean.cpp
+++ examples/CalculateMean.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+/*
+* Function to calculate mean using Fold Expression
+*/
 template <typename ... T>
 auto calmean(T ... ts) {
 	return (ts + ...)/sizeof...(ts);
@@ -8,7 +11,4 @@ auto calmean(T ... ts) {
 
 int main() {
 	std::cout << calmean(5,5,15,15,5,5) << std::endl;
-
-
-
 }
